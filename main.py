d3571c93fa40b8f7370ee316ee27b6d397198bf3
Brilliant Hanabi <moehanabichan@outlook.com>
2024-02-28 22:43:26 +0800

    refactor: Set telegram ids as global variables

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
-import os
 from telegram.ext import ApplicationBuilder, CommandHandler
 from submit import *
+from utils import TG_TOKEN
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -9,11 +9,6 @@ logging.basicConfig(
 )
 
 if __name__ == '__main__':
-    # get args from environment virables
-    TG_TOKEN = os.environ.get('TG_TOKEN')
-    TG_REVIEWER_GROUP = os.environ.get('TG_REVIEWER_GROUP')
-    TG_PUBLISH_CHANNEL = os.environ.get('TG_PUBLISH_CHANNEL')
-
     application = ApplicationBuilder().token(TG_TOKEN).build()
 
     new_submission_handler = CommandHandler('new', handle_new_sumission)
---
+++ utils.py
@@ -0,0 +1,6 @@
+import os
+
+# get args from environment virables
+TG_TOKEN = os.environ.get('TG_TOKEN')
+TG_REVIEWER_GROUP = os.environ.get('TG_REVIEWER_GROUP')
+TG_PUBLISH_CHANNEL = os.environ.get('TG_PUBLISH_CHANNEL')
