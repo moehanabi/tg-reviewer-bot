39a81fbe0ba391a15775c62a80a18e42c2ef0ef9
Brilliant Hanabi <moehanabichan@outlook.com>
2024-08-28 21:28:05 +0800

    feat: Avoid banning a user twice

--- ban.py
+++ ban.py
@@ -20,23 +20,25 @@ async def ban_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
             parse_mode=ParseMode.MARKDOWN_V2,
         )
         return
+    if Banned_user.is_banned(user):
+        await update.message.reply_text(
+            f"{user} 先前已被屏蔽\n"
+            + await get_banned_user_info(
+                context, Banned_user.get_banned_user(user)
+            ),
+            parse_mode=ParseMode.MARKDOWN_V2,
+        )
+        return
 
     username, fullname = await get_name_from_uid(context, user)
     Banned_user.ban_user(
         user, username, fullname, update.effective_user.id, " ".join(result)
     )
     if Banned_user.is_banned(user):
-        banned_user = Banned_user.get_banned_user(user)
-        banned_userinfo = escape_markdown(
-            f"{fullname} ({f'@{username}, ' if username else ''}{user})",
-            version=2,
-        )
-        banned_by_userinfo = escape_markdown(
-            f"{update.effective_user.full_name} (@{update.effective_user.username}, {update.effective_user.id})",
-            version=2,
-        )
         await update.message.reply_text(
-            f"*{banned_userinfo}* 在 *{escape_markdown(str(banned_user['banned_date']), version=2)}* 由 *{banned_by_userinfo}* 因 *{banned_user['banned_reason']}* 屏蔽",
+            await get_banned_user_info(
+                context, Banned_user.get_banned_user(user)
+            ),
             parse_mode=ParseMode.MARKDOWN_V2,
         )
     else:
@@ -44,6 +46,22 @@ async def ban_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
             f"*{user}* 屏蔽失败",
             parse_mode=ParseMode.MARKDOWN_V2,
         )
+
+
+async def get_banned_user_info(context: ContextTypes.DEFAULT_TYPE, user):
+    banned_userinfo = escape_markdown(
+        f"{user.user_fullname} ({f'@{user.user_name}, ' if user.user_name else ''}{user.user_id})",
+        version=2,
+    )
+    banned_by_username, banned_by_fullname = await get_name_from_uid(
+        context, user.banned_by
+    )
+    banned_by_userinfo = escape_markdown(
+        f"{banned_by_fullname} ({banned_by_username}, {user.banned_by})",
+        version=2,
+    )
+    users_string = f"*{banned_userinfo}* 在 *{escape_markdown(str(user['banned_date']), version=2)}* 由 *{banned_by_userinfo}* 因 *{escape_markdown(user['banned_reason'], version=2)}* 屏蔽"
+    return users_string
 
 
 async def unban_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -72,18 +90,7 @@ async def list_banned_users(
     users = Banned_user.get_banned_users()
     users_string = "屏蔽用户列表:\n" if users else "无屏蔽用户\n"
     for user in users:
-        banned_userinfo = escape_markdown(
-            f"{user.user_fullname} ({f'@{user.user_name}, ' if user.user_name else ''}{user.user_id})",
-            version=2,
-        )
-        banned_by_username, banned_by_fullname = await get_name_from_uid(
-            context, user.banned_by
-        )
-        banned_by_userinfo = escape_markdown(
-            f"{banned_by_fullname} (@{banned_by_username}, {user.banned_by})",
-            version=2,
-        )
-        users_string += f"\- *{banned_userinfo}* 在 *{escape_markdown(str(user['banned_date']), version=2)}* 由 *{banned_by_userinfo}* 因 *{user['banned_reason']}* 屏蔽\n"
+        users_string += f"\- {await get_banned_user_info(context, user)}\n"
     await update.message.reply_text(
         users_string,
         parse_mode=ParseMode.MARKDOWN_V2,
