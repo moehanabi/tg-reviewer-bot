1cfd8726953cc96038452bcfe07b8ffa8080202e
CacheCi <155388387+cacheci@users.noreply.github.com>
2025-11-28 23:05:39 +0800

    fix: Too long ban list fail to show

--- ban.py
+++ ban.py
@@ -96,8 +96,16 @@ async def list_banned_users(
     update: Update, context: ContextTypes.DEFAULT_TYPE
 ):
     users = Banned_user.get_banned_users()
-    users_string = "屏蔽用户列表:\n" if users else "无屏蔽用户\n"
+    list_banned_users_page_count = 1
+    users_string = ("屏蔽用户列表:\n页面" + str(list_banned_users_page_count) + "\n") if users else "无屏蔽用户\n"
     for user in users:
+        if len(users_string + f"\- {await get_banned_user_info(context, user)}\n") >= 1300:
+            await update.message.reply_text(
+                users_string,
+                parse_mode=ParseMode.MARKDOWN_V2,
+            )
+            list_banned_users_page_count += 1
+            users_string = "屏蔽用户列表:\n页面" + str(list_banned_users_page_count) + "\n"
         users_string += f"\- {await get_banned_user_info(context, user)}\n"
     await update.message.reply_text(
         users_string,
