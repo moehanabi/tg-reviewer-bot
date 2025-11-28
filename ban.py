d59f2f538a722c11081542730f603cbe4112be17
CacheCi <155388387+cacheci@users.noreply.github.com>
2025-11-28 23:46:48 +0800

    Update ban.py

--- ban.py
+++ ban.py
@@ -99,14 +99,15 @@ async def list_banned_users(
     list_banned_users_page_count = 1
     users_string = ("屏蔽用户列表:\n页面" + str(list_banned_users_page_count) + "\n") if users else "无屏蔽用户\n"
     for user in users:
-        if len(users_string + f"\- {await get_banned_user_info(context, user)}\n") >= 1300:
+        new_banned_usr_str = f"\- {await get_banned_user_info(context, user)}\n"
+        if len(users_string + new_banned_usr_str) >= 1300:
             await update.message.reply_text(
                 users_string,
                 parse_mode=ParseMode.MARKDOWN_V2,
             )
             list_banned_users_page_count += 1
             users_string = "屏蔽用户列表:\n页面" + str(list_banned_users_page_count) + "\n"
-        users_string += f"\- {await get_banned_user_info(context, user)}\n"
+        users_string += new_banned_usr_str
     await update.message.reply_text(
         users_string,
         parse_mode=ParseMode.MARKDOWN_V2,
