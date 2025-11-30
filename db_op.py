c09b67fedd31059fdd3eebc4b62ef2c4b36312c1
CacheCi <155388387+cacheci@users.noreply.github.com>
2025-11-30 19:36:44 +0800

    feat: Optimize listban

--- db_op.py
+++ db_op.py
@@ -15,6 +15,7 @@ from typing_extensions import Annotated
 
 from env import TG_DB_URL
 
+import re
 
 class Base(DeclarativeBase):
     pass
@@ -391,3 +392,8 @@ class DB:
 
 
 db = DB(TG_DB_URL)
+
+def sanitize_userinfo(text: str) -> str:
+    if not text:
+        return ""
+    return re.sub(r'[\u200B-\u200F\u202A-\u202E\u2060-\u206F\uFEFF\x00-\x1F\x7F\x80-\x9F]', '', text).strip()
