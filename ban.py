b6886f5610e661849df9d208d4aec375996fe06a
CacheCi <155388387+cacheci@users.noreply.github.com>
2025-11-30 19:53:14 +0800

    feat: Allow ban and unban with tag

--- ban.py
+++ ban.py
@@ -14,6 +14,12 @@ async def ban_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
         return
     user, result = context.args[0], context.args[1:]
+    if user.startswith(("#USER_","#SUBMITTER_")):
+        if user.startswith("#USER_"):
+            user = user[6:]
+        elif user.startswith("#SUBMITTER_"):
+            user = user[11:]
+
     if not user.isdigit():
         await update.message.reply_text(
             f"ID *{escape_markdown(user,version=2,)}* 无效",
@@ -90,6 +96,20 @@ async def unban_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
         return
     user = context.args[0]
+
+    if user.startswith(("#USER_","#SUBMITTER_")):
+        if user.startswith("#USER_"):
+            user = user[6:]
+        elif user.startswith("#SUBMITTER_"):
+            user = user[11:]
+
+    if not user.isdigit():
+        await update.message.reply_text(
+            f"ID *{escape_markdown(user,version=2,)}* 无效",
+            parse_mode=ParseMode.MARKDOWN_V2,
+        )
+        return
+
     Banned_user.unban_user(user)
     if Banned_user.is_banned(user):
         await update.message.reply_text(
