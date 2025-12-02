efb6b5c669d65da9f7111dff1384e1cebe817230
CacheCi <155388387+cacheci@users.noreply.github.com>
2025-12-02 15:36:37 +0800

    fix: Reviewer now can check own reviewer_stats in pm

--- main.py
+++ main.py
@@ -169,8 +169,7 @@ if __name__ == "__main__":
             CommandHandler(
                 "reviewer_stats",
                 reviewer_stats,
-                filters=~filters.UpdateType.EDITED_MESSAGE
-                & filters.Chat(chat_id=int(TG_REVIEWER_GROUP)),
+                filters=~filters.UpdateType.EDITED_MESSAGE,
             ),
             CommandHandler(
                 "limit",
